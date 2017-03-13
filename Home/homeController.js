53c87eada8ad86fdbe71f50b6345fdd865d9e019
ThaPlayaSlaya <thaplayaslaya@gmail.com>
2017-03-12 20:48:26 -0700

    Cleaning up project environment.

--- Home/homeController.js
+++
@@ -1,8 +0,0 @@
-app.controller('homeController',
-['$scope', '$state', 'movieLists',
-function(scope, $state, movieLists) {
-   
-   console.log(JSON.stringify(movieLists))
-   
-   scope.movieLists = movieLists;
-}])
--- movielistapp.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('mainApp', [
-   'ui.router',
-   'ui.bootstrap',
-   'ngSanitize' 
-]);
